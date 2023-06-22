565a6be4a1606211e908991453822da78eb41082
LaChaznie Moore <lachazniemoore@LaChaznies-MacBook-Air.local>
2023-06-21 23:48:58 -0400

    update js

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
-// Import the logo generator script
-const logoGenerator = require('./logo-generator');
+const inquirer = require('inquirer');
+const { create } = require('svg-captcha');
+const fs = require('fs');
 
-// Call the logo generator function
-logoGenerator.generateLogo();
+// Prompt user for input
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter the text for your logo:',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Select a shape for your logo:',
+      choices: ['circle', 'square', 'triangle'],
+    },
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Enter the color for your logo (hex value or name):',
+    },
+  ])
+  .then((answers) => {
+    const captcha = create({
+      size: 4,
+      ignoreChars: '0o1i',
+      noise: 2,
+      color: true,
+      background: '#ffffff',
+      width: 200,
+      height: 80,
+    });
+
+    // Customize the SVG logo based on user input
+    captcha.text = answers.text;
+    captcha.width = 200;
+    captcha.height = 80;
+    captcha.fontSize = 50;
+    captcha.charPreset = 'ABCDEFGHKLMNPRSTUVWXYZ0123456789';
+
+    // Save the SVG file
+    fs.writeFileSync('logo.svg', captcha.data);
+
+    console.log('Logo generated successfully! Check the "logo.svg" file.');
+  })
+  .catch((error) => {
+    console.error('An error occurred:', error);
+  });
+
+
+
+
+
+
+
